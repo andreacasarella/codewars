0663373ec02d488646d90ef42c2e49c7fa927f0f
Andrea Casarella <andreacasarella@gmail.com>
2020-06-30 01:02:45 +0200

    v.0.0.2

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    roots: ['<rootDir>/exercises'],
+    roots: ["<rootDir>/kata"],
     transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+        "^.+\\.tsx?$": "ts-jest",
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  }
-  
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
